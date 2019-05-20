55ba8e1ad334af7fb7db3f6ff4ac9880411caa77
BenPoch <46890326+BenPoch@users.noreply.github.com>
2019-05-20 15:27:30 +0200

    UI

---
+++ PFAElementals/Assets/Scripts/Decompte.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Decompte : MonoBehaviour
+{
+
+    public Text counterText;
+    public float seconds, minutes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterText = GetComponent<Text>() as Text;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minutes = (int)(Time.time / 60f);
+        seconds = (int)(Time.time % 60f);
+        counterText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
