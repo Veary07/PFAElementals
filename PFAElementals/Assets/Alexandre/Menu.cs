b2a8347a449274ec890ae751a4f97707a9d20e86
arpheons <alexandre.gamedesign@gmail.com>
2019-06-13 11:37:03 +0200

    3

--- PFAElementals/Assets/Alexandre/Menu.cs
+++ PFAElementals/Assets/Alexandre/Menu.cs
@@ -5,12 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] RectTransform triangle;
+    [SerializeField] RectTransform curseur;
+    [SerializeField] RectTransform curseurOptions;
+    [SerializeField] GameObject optionsMenu;
 
+    Vector3 playButton = new Vector3(-250, 100, 0);
+    Vector3 optionsButton = new Vector3(-250, 0, 0);
+    Vector3 quitButton = new Vector3(-250, -100, 0);
 
-    Vector3 high = new Vector3(-250, 100, 0);
-    Vector3 middle = new Vector3(-250, 0, 0);
-    Vector3 down = new Vector3(-250, -100, 0);
+    Vector3 fullScreenButton = new Vector3(-165, 140, 0);
+    Vector3 resolutionButton = new Vector3(-440, 30, 0);
+    Vector3 graphicsButton = new Vector3(-440, -80, 0);
+    Vector3 volumeButton = new Vector3(-440, -210, 0);
+
+    public SettingsMenu settings;
+
+    private bool options = false;
+    private bool fullscreen = true;
 
 
     private int placement = 1;
@@ -18,6 +29,12 @@ public class Menu : MonoBehaviour
 
     [SerializeField] float t = 1f;
 
+    private void Start()
+    {
+        optionsMenu.SetActive(false);
+        curseur.localPosition = playButton;
+        curseurOptions.localPosition = fullScreenButton;
+    }
 
     // Update is called once per frame
     void Update()
@@ -29,13 +46,13 @@ public class Menu : MonoBehaviour
                 case 1:
                     if (Input.GetAxisRaw("VerticalP") < 0)
                     {
-                        triangle.localPosition= Vector3.Lerp(transform.position,middle, t);
+                        curseur.localPosition = optionsButton;
                         placement = 2;
                         axisUsed = true;
                     }
                     else if (Input.GetAxisRaw("VerticalP") > 0)
                     {
-                        triangle.localPosition = Vector3.Lerp(transform.position, down, t);
+                        curseur.localPosition = quitButton;
                         placement = 3;
                         axisUsed = true;
                     }
@@ -48,28 +65,34 @@ public class Menu : MonoBehaviour
                 case 2:
                     if (Input.GetAxisRaw("VerticalP") < 0)
                     {
-                        triangle.localPosition = Vector3.Lerp(transform.position, down, t);
+                        curseur.localPosition = quitButton;
                         placement = 3;
                         axisUsed = true;
                     }
                     else if (Input.GetAxisRaw("VerticalP") > 0)
                     {
-                        triangle.localPosition = Vector3.Lerp(transform.position, high, t);
+                        curseur.localPosition = playButton;
                         placement = 1;
                         axisUsed = true;
+                    }
+                    else if (Input.GetKeyDown("joystick 1 button 0"))
+                    {
+                        optionsMenu.SetActive(true);
+                        options = true;
+                        placement = 4;
                     }
                     break;
 
                 case 3:
                     if (Input.GetAxisRaw("VerticalP") < 0)
                     {
-                        triangle.localPosition = Vector3.Lerp(transform.position, high, t) ;
+                        curseur.localPosition = playButton;
                         placement = 1;
                         axisUsed = true;
                     }
                     else if (Input.GetAxisRaw("VerticalP") > 0)
                     {
-                        triangle.localPosition = Vector3.Lerp(transform.position, middle, t);
+                        curseur.localPosition = optionsButton;
                         placement = 2;
                         axisUsed = true;
                     }
@@ -78,6 +101,96 @@ public class Menu : MonoBehaviour
                         Application.Quit();
                     }
                     break;
+
+                case 4:
+                    if (Input.GetAxisRaw("VerticalP") < 0)
+                    {
+                        curseurOptions.localPosition = resolutionButton;
+                        placement = 5;
+                        axisUsed = true;
+                    }
+                    else if (Input.GetAxisRaw("VerticalP") > 0)
+                    {
+                        curseurOptions.localPosition = volumeButton;
+                        placement = 7;
+                        axisUsed = true;
+                    }
+                    else if (Input.GetKeyDown("joystick 1 button 0"))
+                    {
+                        fullscreen = !fullscreen;
+                        settings.SetFullScreen(fullscreen);
+                    }
+                    break;
+
+                case 5:
+                    if (Input.GetAxisRaw("VerticalP") < 0)
+                    {
+                        curseurOptions.localPosition = graphicsButton;
+                        placement = 6;
+                        axisUsed = true;
+                    }
+                    else if (Input.GetAxisRaw("VerticalP") > 0)
+                    {
+                        curseurOptions.localPosition = fullScreenButton;
+                        placement = 4;
+                        axisUsed = true;
+                    }
+                    else if (Input.GetKeyDown("joystick 1 button 0"))
+                    {
+                        Application.Quit();
+                    }
+                    break;
+
+                case 6:
+                    if (Input.GetAxisRaw("VerticalP") < 0)
+                    {
+                        curseurOptions.localPosition = volumeButton;
+                        placement = 7;
+                        axisUsed = true;
+                    }
+                    else if (Input.GetAxisRaw("VerticalP") > 0)
+                    {
+                        curseurOptions.localPosition = resolutionButton;
+                        placement = 5;
+                        axisUsed = true;
+                    }
+                    else if (Input.GetKeyDown("joystick 1 button 0"))
+                    {
+                        Application.Quit();
+                    }
+                    break;
+
+                case 7:
+                    if (Input.GetAxisRaw("VerticalP") < 0)
+                    {
+                        curseurOptions.localPosition = fullScreenButton;
+                        placement = 4;
+                        axisUsed = true;
+                    }
+                    else if (Input.GetAxisRaw("VerticalP") > 0)
+                    {
+                        curseurOptions.localPosition = graphicsButton;
+                        placement = 6;
+                        axisUsed = true;
+                    }
+                    else if (Input.GetKeyDown("joystick 1 button 0"))
+                    {
+                        Application.Quit();
+                    }
+                    break;
+
+            }
+        }
+
+        if(options)
+        {
+            if (Input.GetKeyDown("joystick 1 button 1"))
+            {
+                curseur.localPosition = playButton;
+                curseurOptions.localPosition = fullScreenButton;
+                optionsMenu.SetActive(false);
+                options = false;
+                placement = 1;
             }
         }
     }
