6034e761ba45b979c8a9480c3c1424e0191b203c
arpheons <alexandre.gamedesign@gmail.com>
2019-06-14 09:58:13 +0200

    changement assets

---
+++ PFAElementals/Assets/Scripts/Systems/Swap.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swap : MonoBehaviour
+{
+    [SerializeField] GameObject summer;
+    [SerializeField] GameObject winter;
+    GameObject child;
+
+    Material map;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = GameObject.Find("Ground").GetComponent<Renderer>().material;
+
+        if (this.transform.position.x * 0.5f < -map.GetFloat("Vector1_3ECABBA8"))
+        {
+            summer.SetActive(true);
+            winter.SetActive(false);
+        }
+        else if (this.transform.position.x * 0.5f > -map.GetFloat("Vector1_3ECABBA8"))
+        {
+            summer.SetActive(false);
+            winter.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x * 0.5f < -map.GetFloat("Vector1_3ECABBA8"))
+        {
+            summer.SetActive(true);
+            winter.SetActive(false);
+        }
+        else if (this.transform.position.x * 0.5f > -map.GetFloat("Vector1_3ECABBA8"))
+        {
+            summer.SetActive(false);
+            winter.SetActive(true);
+        }
+    }
+}
